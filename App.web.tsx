147ef72a1e6a8dc8a0d3bf6fe96e12a91ebf2626
ohsugi5532 <t.ohsugi5532@gmail.com>
2021-09-04 06:48:35 +0900

    Intialize

---
+++ App.web.tsx
@@ -0,0 +1,305 @@
+import React, {Fragment} from 'react';
+import {View, Text, StyleSheet, useColorScheme, SafeAreaView, StatusBar, ScrollView, Platform, TouchableOpacity, ImageBackground, Linking} from 'react-native';
+
+// Copy from react-native/Libraries/NewAppScreen
+const Colors = {
+  primary: '#1292B4',
+  white: '#FFF',
+  lighter: '#F3F3F3',
+  light: '#DAE1E7',
+  dark: '#444',
+  darker: '#222',
+  black: '#000',
+}
+
+const links = [
+  {
+    id: 1,
+    title: 'The Basics',
+    link: 'https://reactnative.dev/docs/tutorial',
+    description: 'Explains a Hello World for React Native.',
+  },
+  {
+    id: 2,
+    title: 'Style',
+    link: 'https://reactnative.dev/docs/style',
+    description:
+      'Covers how to use the prop named style which controls the visuals.',
+  },
+  {
+    id: 3,
+    title: 'Layout',
+    link: 'https://reactnative.dev/docs/flexbox',
+    description: 'React Native uses flexbox for layout, learn how it works.',
+  },
+  {
+    id: 4,
+    title: 'Components',
+    link: 'https://reactnative.dev/docs/components-and-apis',
+    description: 'The full list of components and APIs inside React Native.',
+  },
+  {
+    id: 5,
+    title: 'Navigation',
+    link: 'https://reactnative.dev/docs/navigation',
+    description:
+      'How to handle moving between screens inside your application.',
+  },
+  {
+    id: 6,
+    title: 'Networking',
+    link: 'https://reactnative.dev/docs/network',
+    description: 'How to use the Fetch API in React Native.',
+  },
+  {
+    id: 7,
+    title: 'Help',
+    link: 'https://reactnative.dev/help',
+    description:
+      'Need more help? There are many other React Native developers who may have the answer.',
+  },
+  {
+    id: 8,
+    title: 'Follow us on Twitter',
+    link: 'https://twitter.com/reactnative',
+    description:
+      'Stay in touch with the community, join in on Q&As and more by following React Native on Twitter.',
+  },
+];
+
+const Header: React.FC = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <ImageBackground
+      accessibilityRole="image"
+      source={require('./web/logo.png')}
+      style={[
+        headerStyles.background,
+        {
+          backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+        },
+      ]}
+      imageStyle={headerStyles.logo}>
+      <Text
+        style={[
+          headerStyles.text,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        Welcome to
+        {'\n'}
+        React Native
+      </Text>
+    </ImageBackground>
+  );
+};
+
+const LinkList: React.FC = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={linkListStyles.container}>
+      {links.map(({id, title, link, description}) => (
+        <Fragment key={id}>
+          <View
+            style={[
+              linkListStyles.separator,
+              {
+                backgroundColor: isDarkMode ? Colors.dark : Colors.light,
+              },
+            ]}
+          />
+          <TouchableOpacity
+            accessibilityRole="button"
+            onPress={() => Linking.openURL(link)}
+            style={linkListStyles.linkContainer}>
+            <Text style={linkListStyles.link}>{title}</Text>
+            <Text
+              style={[
+                linkListStyles.description,
+                {
+                  color: isDarkMode ? Colors.lighter : Colors.dark,
+                },
+              ]}>
+              {description}
+            </Text>
+          </TouchableOpacity>
+        </Fragment>
+      ))}
+    </View>
+  );
+};
+
+const DebugInstructions: React.FC = Platform.select({
+  ios: () => (
+    <Text>
+      Press <Text style={styles.highlight}>Cmd + D</Text> in the simulator or{' '}
+      <Text style={styles.highlight}>Shake</Text> your device to open the React
+      Native debug menu.
+    </Text>
+  ),
+  default: () => (
+    <Text>
+      Press <Text style={styles.highlight}>Cmd or Ctrl + M</Text> or{' '}
+      <Text style={styles.highlight}>Shake</Text> your device to open the React
+      Native debug menu.
+    </Text>
+  ),
+});
+
+const ReloadInstructions: React.FC = Platform.select({
+  ios: () => (
+    <Text>
+      Press <Text style={styles.highlight}>Cmd + R</Text> in the simulator to
+      reload your app's code.
+    </Text>
+  ),
+  default: () => (
+    <Text>
+      Double tap <Text style={styles.highlight}>R</Text> on your keyboard to
+      reload your app's code.
+    </Text>
+  ),
+});
+
+const Section: React.FC<{
+  title: string;
+}> = ({children, title}) => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LinkList />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+const headerStyles = StyleSheet.create({
+  background: {
+    paddingBottom: 40,
+    paddingTop: 96,
+    paddingHorizontal: 32,
+  },
+  logo: {
+    opacity: 0.2,
+    overflow: 'visible',
+    resizeMode: 'cover',
+    /*
+     * These negative margins allow the image to be offset similarly across screen sizes and component sizes.
+     *
+     * The source logo.png image is 512x512px, so as such, these margins attempt to be relative to the
+     * source image's size.
+     */
+    marginLeft: -128,
+    marginBottom: -192,
+  },
+  text: {
+    fontSize: 40,
+    fontWeight: '700',
+    textAlign: 'center',
+  },
+});
+
+const linkListStyles = StyleSheet.create({
+  container: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  linkContainer: {
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 8,
+  },
+  link: {
+    flex: 2,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.primary,
+  },
+  description: {
+    flex: 3,
+    paddingVertical: 16,
+    fontWeight: '400',
+    fontSize: 18,
+  },
+  separator: {
+    height: StyleSheet.hairlineWidth,
+  },
+});
+
+export default App;
